555e1c518d667e8519e506d4be9e451ac6217da9
DerrickHa <65101778+DerrickHa@users.noreply.github.com>
2022-10-07 14:33:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function dir()
+function dirniagara()
 {
     let road = document.querySelector("#road").value;
     let city = document.querySelector("#city").value;
-    window.open("https://www.google.ca/maps/dir/" + road + ", +" + city+"/Bayview+Secondary+School,+10077+Bayview+Ave,+Richmond+Hill,+ON+L4C+2L4")
-} 
+    window.open("https://google.ca/maps/dir/" + road + ", " + city + "/Bayview+Secondary+School/")
+}
